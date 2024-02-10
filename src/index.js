1f4db0a9d1abaedea1068b9a621414117c704b3c
Tebogo Dikgatlong <97123206+Dikgatlong27@users.noreply.github.com>
2024-02-10 19:49:10 +0200

    polyfills added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import 'whatwg-fetch';
+import 'es6-object-assign';
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
