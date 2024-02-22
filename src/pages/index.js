46c801a823c4038ba94dabcd360bc298a7169860
Tebogo Dikgatlong <97123206+Dikgatlong27@users.noreply.github.com>
2024-02-22 17:59:36 +0200

    modified pages.

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Home from './WebPages/Home';
+import About from './WebPages/About';
+import Contact from './WebPages/Contact';
+import Menu from './WebPages/Menu';
+import Gallery from './WebPages/Gallery';
+
+export {
+    Home,
+    About,
+    Contact,
+    Menu,
+    Gallery,
+};
