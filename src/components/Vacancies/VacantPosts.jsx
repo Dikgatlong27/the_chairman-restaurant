f6b2015ce30d985bff5a2713cf74a57e96d5c25b
Tebogo Dikgatlong <97123206+Dikgatlong27@users.noreply.github.com>
2024-04-26 19:07:24 +0200

    modified VacantPosts

--- src/components/Vacancies/VacantPosts.jsx
+++ src/components/Vacancies/VacantPosts.jsx
@@ -10,7 +10,7 @@ const VacantPosts = () => {
         <div className="slide">
             <h3 className='p__cursive'>Sues Chef</h3>
             <p className='p__opensans'>Produces high quality plates, including both design and taste. Oversees and supervises kitchen staff. Assists with menu planning, inventory, and management of supplies. Ensures that food is top quality and that kitchen is in good condition.</p>
-            <Link to="/vacancies/job1" className="custom__button">Apply</Link>
+            
         </div>
     </div>
   )
