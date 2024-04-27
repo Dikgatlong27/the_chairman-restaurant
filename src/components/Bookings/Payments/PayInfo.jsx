aebc74125d12719bd188ab5377b26a01fbb30aff
Tebogo Dikgatlong <97123206+Dikgatlong27@users.noreply.github.com>
2024-04-27 18:33:51 +0200

    Modified PayInfo

--- src/components/Bookings/Payments/PayInfo.jsx
+++ src/components/Bookings/Payments/PayInfo.jsx
@@ -14,7 +14,7 @@ const PayInfo = () => {
           <p className="POP">Send POP to <a className="POP__email" href='mailto:social@chairmanforte.co.za'>social@chairmanforte.co.za</a></p>
       </div>
       <p className="app__payInfo-conditions_header">Payment Terms & Conditions:</p>
-      <p className="app__payInfo-conditions">Booking Confirmation: A non-refundable deposit of R1000 is required for reservations with more than 10 people.<br/> Deposit Payment: The deposit will be deducted from the final bill.<br/> Final Payment: The remaining balance must be settled on the reservation day.<br/> Additional Charges: Any extra services or items will be added to the final bill.<br/> Payment Methods: Cash or direct deposits.<br/> Service Charge: A discretionary service charge may apply for larger groups.<br/> Agreement: By booking and paying the deposit, you agree to the terms and conditions.</p>
+      <p className="app__payInfo-conditions">Booking Confirmation: A non-refundable deposit of R3500 is required for reservations with more than 10 people.<br/> Deposit Payment: The deposit will be deducted from the final bill.<br/> Final Payment: The remaining balance must be settled on the reservation day.<br/> Additional Charges: Any extra services or items will be added to the final bill.<br/> Payment Methods: Cash or direct deposits.<br/> Service Charge: A discretionary service charge may apply for larger groups.<br/> Agreement: By booking and paying the deposit, you agree to the terms and conditions.</p>
     </>
     
   )
