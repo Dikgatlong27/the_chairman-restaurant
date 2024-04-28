b72adf89e3d381e518cc1dd5fdb0cb1feb1afe6d
Tebogo Dikgatlong <97123206+Dikgatlong27@users.noreply.github.com>
2024-04-28 23:31:37 +0200

    modified vacancies ad at home

--- src/components/Vacancies/VacanciesPostsAtHome.jsx
+++ src/components/Vacancies/VacanciesPostsAtHome.jsx
@@ -8,7 +8,7 @@ const VacanciesPostsAtHome = () => {
   return (
     <div className="sliding-text-container" style={{background:'#111'}}>        
         <h2>Featured Job Openings</h2>
-        <p className="p__opensans sliding-text">Join us at our newest branch at The Gables Mall in Eswatini! Explore available opportunities on our vacancies page.</p>
+        <p className="p__opensans sliding-text">Join us at our newest branch at The Gables Mall in Eswatini! Explore available opportunities on our vacancies page. Explore available opportunities on our vacancies page. </p>
         <li className='vacancies-btn_mobile'><Link to='/vacancies'>See Posts</Link></li>
     </div>
   )
